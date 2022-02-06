37eaba9c4cd589d9557e98bd66b819277128ff23
JustinWong98 <justinwong8991@gmail.com>
2022-02-06 15:27:14 +0800

    new html and js template

---
+++ script2.js
@@ -0,0 +1,15 @@
+
+// Example
+// Remember that accurate and concise naming is key for your functions. You should be able to tell what the function is supposed to do based off its name alone.
+
+// Q1. If we input the number of pizzas we have, return the number of slices you would have if each pizza could be split into 8 slices.
+var pizzaSlices = function (input) {
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+// Q2. Fahrenheit to Celsius
+var fahrenheitToCelcius = function (input) {
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
