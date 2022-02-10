8b00a67d3ad7300719ab5aeb35a46987f1864263
Lee Chuan Xin <chuanxin@gametize.com>
2022-02-11 00:43:39 +0800

    feat: day 5 ice.

--- day05-arrays-and-loops/in-class/script.js
+++ day05-arrays-and-loops/in-class/script.js
@@ -1,4 +1,35 @@
-var main = function (input) {
+var madLibsAdjectivesMain = function (input) {
+  // Complete the Base: Mad Libs Adjectives exercise below with madLibsAdjectivesMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var madLibsPopularMain = function (input) {
+  // Complete the Comfortable: Popular Mad Libs exercise below with madLibsPopularMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var madLibsInputCreateMain = function (input) {
+  // Complete the Comfortable: Input and Create Mode exercise below with madLibsInputCreateMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var madLibsMultipleWordsMain = function (input) {
+  // Complete the Comfortable: Input Multiple Words exercise below with madLibsMultipleWordsMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var madLibsMultipleTypesMain = function (input) {
+  // Complete the More Comfortable: Mad Libs Multiple Word Types exercise below with madLibsMultipleTypesMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var madLibsSetsMain = function (input) {
+  // Complete the More Comfortable: Sets of Mad Libs exercise below with madLibsSetsMain as the main function.
   var myOutputValue = 'hello world';
   return myOutputValue;
 };
