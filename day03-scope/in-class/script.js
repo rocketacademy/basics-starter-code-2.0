5471a40ea63757793e186f9e9a6490b862b9fb3d
Lee Chuan Xin <chuanxin@gametize.com>
2022-02-10 19:13:04 +0800

    feat: day 3 in class till secret word twice in a row 2.

--- day03-scope/in-class/script.js
+++ day03-scope/in-class/script.js
@@ -1,4 +1,35 @@
-var main = function (input) {
+var secretWordBaseMain = function (input) {
+  // Complete the Base: Secret Word exercise below with secretWordBaseMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var secretWordTwiceRowMain = function (input) {
+  // Complete the Comfortable: Secret Word Twice in a Row exercise below with secretWordTwiceRowMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var secretWordXRowMain = function (input) {
+  // Complete the Comfortable: Secret Word X in a Row exercise below with secretWordXRowMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var diceWithinMain = function (input) {
+  // Complete the More Comfortable: Dice Within exercise below with diceWithinMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var diceWithin2DiceMain = function (input) {
+  // Complete the More Comfortable: Dice Within with 2 Dice exercise below with diceWithin2DiceMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var dice4DMain = function (input) {
+  // Complete the More Comfortable: Dice 4D exercise below with dice4DeMain as the main function.
   var myOutputValue = 'hello world';
   return myOutputValue;
 };
