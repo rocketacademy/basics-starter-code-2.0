399acf91a075210044b38da4f7170b9996a1efbc
Lee Chuan Xin <chuanxin@gametize.com>
2022-02-10 18:42:05 +0800

    feat: day2 in class exercises.

---
+++ day02-logic-and-control-flow/in-class/script.js
@@ -0,0 +1,35 @@
+var lucky11Main = function (input) {
+  // Complete the Base: Lucky 11 exercise below with lucky11Main as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var hawkerFoodCategorisationMain = function (input) {
+  // Complete the Base: Hawker Food Categorisation exercise below with hawkerFoodCategorisationMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var fourDSingleDigitMain = function (input) {
+  // Complete the Comfortable: 4D with Single-Digit Comparison exercise below with fourDSingleDigitMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var hawkerFoodRandomnessMain = function (input) {
+  // Complete the Comfortable: Hawker Food Randomness exercise below with hawkerFoodRandomnessMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var fourDWinningRangeMain = function (input) {
+  // Complete the More Comfortable: 4D with Winning Range exercise below with fourDWinningRangeMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var hawkerFoodOmakaseMain = function (input) {
+  // Complete the More Comfortable: Hawker Food Omakase exercise below with hawkerFoodOmakaseMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
