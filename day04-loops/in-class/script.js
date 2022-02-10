3cff4e11a46e3240ff295ec81c7945142c77015d
Lee Chuan Xin <chuanxin@gametize.com>
2022-02-11 00:06:24 +0800

    feat: day 4 emoji drawing ice.

--- day04-loops/in-class/script.js
+++ day04-loops/in-class/script.js
@@ -1,4 +1,29 @@
-var main = function (input) {
+var emojiNumberCharactersMain = function (input) {
+  // Complete the Base: Emoji Drawing Number of Characters exercise below with emojiNumberCharactersMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var emojiSquareMain = function (input) {
+  // Complete the Base: Emoji Drawing Square exercise below with emojiSquareMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var emojiTriangleMain = function (input) {
+  // Complete the Comfortable: Emoji Drawing Triangle exercise below with emojiTriangleMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var emojiOutlineSquareMain = function (input) {
+  // Complete the More Comfortable: Emoji Drawing Outline Square exercise below with emojiOutlineSquareMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var emojiCenterSquareMain = function (input) {
+  // Complete the More Comfortable: Emoji Drawing Center Square exercise below with emojiCenterSquareMain as the main function.
   var myOutputValue = 'hello world';
   return myOutputValue;
 };
