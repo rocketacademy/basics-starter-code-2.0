538e1643896ae1dd9c9e426eeca9aa999ffd8c09
Lee Chuan Xin <chuanxin@gametize.com>
2022-02-13 23:33:50 +0800

    feat: day7 ice moar cards.

---
+++ day07-moar-cards/in-class/script.js
@@ -0,0 +1,59 @@
+var moarCardsSingleCardMain = function (input) {
+  // Complete the Base: Moar Cards Display Single Card exercise below with moarCardsSingleCardMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var moarCardsLowCardMain = function (input) {
+  // Complete the Base: Moar Cards Low Card exercise below with moarCardsLowCardMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var moarCardsLowCardQueenWinnerMain = function (input) {
+  // Complete the Base: Moar Cards Low Card with Queen Winner exercise below with moarCardsLowCardQueenWinnerMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var moarCardsLowCardHandsMain = function (input) {
+  // Complete the Base: Moar Cards Low Card Hands exercise below with moarCardsLowCardHandsMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var moarCardsLowCardSuitMain = function (input) {
+  // Complete the Base: Moar Cards Low Card Suit Output exercise below with moarCardsLowCardSuitMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var moarCardsLowCardWildCardMain = function (input) {
+  // Complete the More Comfortable: Moar Cards Low Card with Wild Card exercise below with moarCardsLowCardWildCardMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var moarCardsLowCardWildPlayerMain = function (input) {
+  // Complete the More Comfortable: Moar Cards Low Card with Player-Chosen Wild Card exercise below with moarCardsLowCardWildPlayerMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var moarCardsLowCardBetsMain = function (input) {
+  // Complete the More Comfortable: Moar Cards Low Card with Bets exercise below with moarCardsLowCardBetsMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var moarCardsLowCard2PMain = function (input) {
+  // Complete the More Comfortable: Moar Cards Low Card 2-Player Mode exercise below with moarCardsLowCard2PMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
+
+var moarCardsLowCard2PairsMain = function (input) {
+  // Complete the More Comfortable: Moar Cards Low Card 2-Player Mode with Pairs exercise below with moarCardsLowCard2PMain as the main function.
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
