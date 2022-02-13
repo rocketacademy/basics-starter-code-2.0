eef633e4788e3d1d21e93560a0e9c1dec58a9390
Lee Chuan Xin <chuanxin@gametize.com>
2022-02-14 03:23:46 +0800

    feat: day 1 train speed pce ref soln.

--- day01-data-manipulation-and-functions/pre-class/script.js
+++ day01-data-manipulation-and-functions/pre-class/script.js
@@ -21,9 +21,42 @@ var functionsExampleMain = function (input) {
   return myOutputValue;
 };
 
+/**
+ * Comfortable: Train Speed
+ */
+
+/*
+  HELPER FUNCTIONS
+*/
+var convertMinsToHrs = function (mins) {
+  return mins / 60;
+};
+
+var calcTrainTwoSpeed = function (delayInMins) {
+  // save the fixed values to variables
+  var trainSpeed = 200;
+  var timeToTokyo = 2;
+  // calculate the distance to tokyo
+  // relevant mathematical formula to note: dist = speed x time
+  var distanceToTokyo = trainSpeed * timeToTokyo;
+
+  // calculate new time available for travel after subtracting the delay
+  var remainingTimeForTravel = timeToTokyo - convertMinsToHrs(delayInMins);
+
+  // calculate the speed that train 2 needs to travel at, in order to cover the same distance at a reduced travel time
+  trainTwoNewSpeed = distanceToTokyo / remainingTimeForTravel;
+
+  // return the new speed; 'toFixed' limits the ans to a specified decimal place
+  return trainTwoNewSpeed.toFixed(2);
+};
+
+/*
+  MAIN FUNCTION
+*/
 var trainSpeedMain = function (input) {
   // Attempt the Comfortable: Train Speed exercise from the Functions I module below with trainSpeedMain as the main function.
-  var myOutputValue = 'hello world';
+  var trainTwoNewSpeed = calcTrainTwoSpeed(input);
+  var myOutputValue = `Given a ${input}min delay, train 2 needs to travel at a speed of ${trainTwoNewSpeed} km/h in order to arrive at the same time as train 1`;
   return myOutputValue;
 };
 
