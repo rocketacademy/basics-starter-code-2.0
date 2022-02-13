b36437608f396611663e9c883986ab4b1bcecde3
Lee Chuan Xin <chuanxin@gametize.com>
2022-02-14 04:28:57 +0800

    feat: day3 pce red mode ref soln.

--- day03-program-state/pre-class/script.js
+++ day03-program-state/pre-class/script.js
@@ -46,8 +46,32 @@ var followAlongMain = function (input) {
   return myOutputValue;
 };
 
+/**
+ * Red Mode (Program State for Game Modes)
+ */
+
+var mode = 'green';
+
 var redModeMain = function (input) {
   // Attempt the Red Mode exercise from the Program State for Game Modes module below with redModeMain as the main function.
-  var myOutputValue = 'hello world';
+  if (input == 'greenmode') {
+    mode = 'green';
+  } else if (input == 'bluemode') {
+    mode = 'blue';
+  } else if (input == 'redmode') {
+    mode = 'red';
+  }
+
+  var myOutputValue =
+    'A fool sees not the same tree that a wise man sees. -William Blake';
+
+  if (mode == 'blue') {
+    myOutputValue =
+      'The sea, once it casts its spell, holds one in its net of wonder forever. -Jacques Cousteau';
+  }
+  if (mode == 'red') {
+    myOutputValue = 'A drop of ink may make a million think. –Lord Byron';
+  }
+
   return myOutputValue;
 };
