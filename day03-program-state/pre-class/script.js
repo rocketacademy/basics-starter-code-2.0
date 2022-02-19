22ccd6fa4d4776eb82cd056fad68f07dc5fd9959
ennnm <jiaen.1sc4@gmail.com>
2022-02-19 14:29:57 +0800

    feat: pre-class solutions for day3

--- day03-program-state/pre-class/script.js
+++ day03-program-state/pre-class/script.js
@@ -4,39 +4,129 @@ var introScopeMain = function (input) {
   return myOutputValue;
 };
 
+var rollDice = function () {
+  // produces a decimal between 0 and 6
+  var randomDecimal = Math.random() * 6;
+
+  // take off the decimal
+  var randomInteger = Math.floor(randomDecimal);
+
+  // it's a number from 0 - 5 ... add 1
+  var diceNumber = randomInteger + 1;
+
+  return diceNumber;
+};
+
 var appSetupMain = function (input) {
   // Attempt the Base: App Setup exercise from the Program Lifecycle and State module below with appSetupMain as the main function.
-  var myOutputValue = 'hello world';
+  var randomDiceNumber = rollDice();
+
+  var myOutputValue = 'you lose';
+
+  if (randomDiceNumber == input) {
+    myOutputValue = 'you win';
+  }
+
   return myOutputValue;
 };
+
+// Declare global variable for storing 'last rolls' outside the lastRollMain function
+var lastRoll = 0;
 
 var lastRollMain = function (input) {
   // Attempt the Base: Last Roll exercise from the Program Lifecycle and State module below with lastRollMain as the main function.
-  var myOutputValue = 'hello world';
+  var randomDiceNumber = rollDice();
+
+  var myOutputValue = `Your last roll was ${lastRoll}. You just rolled a ${randomDiceNumber}. You guessed ${input}. `;
+
+  if (randomDiceNumber == input) {
+    myOutputValue += 'You win!';
+  } else {
+    myOutputValue += 'You lose!';
+  }
+
+  // store the most recent roll as the new lastRoll
+  lastRoll = randomDiceNumber;
+
   return myOutputValue;
 };
+
+var numGames = 0;
+var numWins = 0;
 
 var winLossMain = function (input) {
   // Attempt the Base: Win / Loss exercise from the Program Lifecycle and State module below with winLossMain as the main function.
-  var myOutputValue = 'hello world';
+  var randomDiceNumber = rollDice();
+  var myOutputValue = '';
+
+  numGames += 1;
+
+  if (randomDiceNumber == input) {
+    myOutputValue = 'You win!';
+    numWins += 1;
+  } else {
+    myOutputValue = 'You lose!';
+  }
+
+  var percentageWins = (numWins / numGames) * 100;
+  myOutputValue += ` Your win percentage is ${percentageWins}%`;
+
   return myOutputValue;
 };
+
+var rollCounter = [0, 0, 0, 0, 0, 0];
+// rollCounter[0] collects count for diceRolls of 1
+// rollCounter[1] collects count for diceRolls of 2
+// rollCounter[x-1] collects count for diceRolls of x
 
 var mostRolledMain = function (input) {
   // Attempt the More Comfortable: Most Rolled exercise from the Program Lifecycle and State module below with mostRolledMain as the main function.
-  var myOutputValue = 'hello world';
+  var randomDiceNumber = rollDice();
+  var rollCounterIndex = randomDiceNumber - 1;
+  rollCounter[rollCounterIndex] += 1;
+
+  var maxNumOfRolls = max(rollCounter);
+  var indexOfMax = rollCounter.indexOf(maxNumOfRolls);
+  var rolledMost = indexOfMax + 1;
+
+  var myOutputValue = `You just rolled a ${randomDiceNumber}. ${rolledMost} is the number you roll the most. You guessed ${input}. `;
+
+  if (randomDiceNumber == input) {
+    myOutputValue += 'You win!';
+  } else {
+    myOutputValue += 'You lose!';
+  }
+
   return myOutputValue;
 };
 
-var guessingMain = function (input) {
+var guessingMain = function (guess) {
   // Attempt the More Comfortable: Guessing exercise from the Program Lifecycle and State module below with guessingMain as the main function.
-  var myOutputValue = 'hello world';
+  var randomDiceNumber = rollDice();
+  var guessDifference = Math.abs(guess - randomDiceNumber);
+  var points = 0;
+
+  if (guessDifference == 0) {
+    points += 2;
+  } else if (guessDifference == 1) {
+    points += 1;
+  }
+  var myOutputValue = `You guessed ${randomDiceNumber} which is off from rolled dice number ${randomDiceNumber} by ${guessDifference}. You win ${points} points!`;
+
   return myOutputValue;
 };
 
 var advancedGuessingMain = function (input) {
   // Attempt the More Comfortable: Advanced Guessing exercise from the Program Lifecycle and State module below with advancedGuessingMain as the main function.
-  var myOutputValue = 'hello world';
+  var randomDiceNumber = rollDice();
+  var guessDifference = Math.abs(guess - randomDiceNumber);
+
+  // finding a pattern between guess difference and their points
+  var totalWinnablePts = 5;
+  var pointsWon = totalWinnablePts - guessDifference;
+
+  var myOutputValue = `You guessed ${randomDiceNumber} which is off from rolled dice number ${randomDiceNumber} by ${guessDifference}. You win ${pointsWon} points!`;
+
   return myOutputValue;
 };
 
