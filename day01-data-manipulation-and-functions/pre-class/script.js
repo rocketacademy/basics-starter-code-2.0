67e78c3e60132b41d6bddaba0b788d8f8db9688b
ennnm <jiaen.1sc4@gmail.com>
2022-02-19 13:35:53 +0800

    featt: day1 preclass ref solutions

--- day01-data-manipulation-and-functions/pre-class/script.js
+++ day01-data-manipulation-and-functions/pre-class/script.js
@@ -5,19 +5,25 @@ var main = function (input) {
 
 var greetingMain = function (input) {
   // Attempt the Greeting Program exercise from the Our First Program module below with greetingMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = 'Hello ' + input + ', you look great today!';
   return myOutputValue;
 };
 
 var metricMain = function (input) {
   // Attempt the Metric Conversion Program exercise from the Our First Program module below with metricMain as the main function.
-  var myOutputValue = 'hello world';
+  var distanceInMiles = input * 0.62;
+
+  var myOutputValue =
+    'Hi! ' + input + ' is equal to ' + distanceInMiles + ' miles.';
   return myOutputValue;
 };
 
 var functionsExampleMain = function (input) {
   // Attempt the Base: Run Example Code exercise from the Functions I module below with functionsExampleMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = convertMinsToHrs();
+  // var myOutputValue = calcTrainTwoSpeed(input);
+  // var myOutputValue = trainSpeedMain(input);
+  // var myOutputValue = clockMain(input);
   return myOutputValue;
 };
 
@@ -62,6 +68,23 @@ var trainSpeedMain = function (input) {
 
 var clockMain = function (input) {
   // Attempt the More Comfortable: Clock exercise from the Functions I module below with clockMain as the main function.
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+
+  // get number of hours and minutes from input
+  var minsPerHour = 60;
+  var numHours = 1 + input / minsPerHour;
+  var numMins = input % minsPerHour;
+
+  // angle of hour hand (from 12)
+  var numberOfHours = 12;
+  var oneRoundAngle = 360;
+  var anglePerHour = oneRoundAngle / numberOfHours;
+  var hourAngle = anglePerHour * numHours;
+
+  // angle of minute hand (from 12)
+  var minutesPerHour = 60;
+  var anglePerMinute = oneRoundAngle / minutesPerHour;
+  var minuteAngle = anglePerMinute * numMins;
+
+  var angleHourAndMin = Math.abs(hourAngle - minuteAngle);
+  return angleHourAndMin;
 };
