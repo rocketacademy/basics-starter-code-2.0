d5b24d2548570ed550761064dc8d0cdb14864773
CodeArtistss <100514432+CodeArtistss@users.noreply.github.com>
2022-04-04 20:37:27 +0800

    initial commit

--- day02-if-statements-boolean-operators/pre-class/script.js
+++ day02-if-statements-boolean-operators/pre-class/script.js
@@ -1,35 +1,38 @@
-var orangeJuiceMain = function (input) {
+var orangeJuiceAdvanced = function (numGuest, glassesPerGuest) {
   // Attempt the Orange Juice Calculator exercise from the Functions II module below with orangeJuiceMain as the main function.
-  var myOutputValue = 'hello world';
+  var orangesPerGlass = 4;
+  var numGlasses = numGuest * glassesPerGuest;
+  var numOranges = numGlasses * orangesPerGlass;
+  var myOutputValue = `${numOranges} oranges are needed`;
   return myOutputValue;
 };
 
 var paintMain = function (input) {
   // Attempt the House Paint exercise from the Functions II module below with paintMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var randomDiceMain = function (input) {
   // Attempt the Random Dice Rolls exercise from the Intro to Logic and Control Flow module below with randomDiceMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var secretPhraseMain = function (input) {
   // Attempt the Secret Phrase exercise from the If, Else, Else If module below with secretPhraseMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var diceGameMain = function (input) {
   // Attempt the Dice Game exercise from the If, Else, Else If module below with diceGameMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var twiceGuessMain = function (input) {
   // Attempt the Twice the Guess exercise from the If, Else, Else If module below with twiceGuessMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
--- project1-sps/script.js
+++ project1-sps/script.js
@@ -1,4 +1,76 @@
+/* What is the input going to be?
+
+User input either scissors paper or stone
+
+// What should the output be?
+user win or lose compared to computer selection
+// How will the computer's option be randomly generated?
+
+// How many different cases are there?
+User input > computer random generate > check if the input matches or win or lose 
+> return message to user win or lose or tie
+*/
+
+//
+
+// initial bankRoll state
+var roundsWon = 0;
+
+// Global Variables Rock, Paper, Scissors
+var stone = 1;
+var paper = 2;
+var scissors = 3;
+
+var generatingComputerChoice = function () {
+  var computerChoiceInt = Math.floor(Math.random() * 3) + 1;
+  return computerChoiceInt;
+};
+
+var convertRandomGenerateToString = function (randomGenerate) {
+  if (randomGenerate === 1) {
+    return "stone";
+  } else if (randomGenerate === 2) {
+    return "paper";
+  } else return "scissors";
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  //console.log(input);
+  if (input !== "scissors" && input !== "paper" && input !== "stone") {
+    let errorMsg = "Hi, you have not input either scissors, paper or stone.";
+    return errorMsg;
+  }
+
+  var inputString = input;
+  if (input === "scissors") input = scissors;
+  else if (input === "paper") input = paper;
+  else input = stone;
+
+  var randomGenerate = generatingComputerChoice();
+  var randomGenerateString = convertRandomGenerateToString(randomGenerate);
+  if (input === randomGenerate) {
+    return "Its a draw!";
+  } else if (
+    (input === stone && randomGenerate === scissors) ||
+    (input === paper && randomGenerate === stone) ||
+    (input === scissors && randomGenerate === paper)
+  ) {
+    roundsWon = roundsWon + 1;
+    let winMsg =
+      "You won!, You selected " +
+      inputString +
+      " and your opponent selected " +
+      randomGenerateString +
+      ". Current rounds won: " +
+      roundsWon;
+    return winMsg;
+  }
+  var lossMsg =
+    "You lose!, You selected " +
+    inputString +
+    " and your opponent selected " +
+    randomGenerateString +
+    ". Current rounds won: " +
+    roundsWon;
+  return lossMsg;
 };
