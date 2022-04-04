3d72cf6bc0c6f07c9afccdac6f349492b1ca1482
shannonssd <83911483+shannonssd@users.noreply.github.com>
2022-04-04 19:34:35 +0800

    initial commit

--- day01-data-manipulation-and-functions/in-class/script.js
+++ day01-data-manipulation-and-functions/in-class/script.js
@@ -1,35 +1,35 @@
 var fahrenheitToCelsiusMain = function (input) {
   // Complete the Base: Fahrenheit to Celsius exercise below with fahrenheitToCelsiusMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var roadTripCostBaseMain = function (input) {
   // Complete the Base: Road Trip Cost exercise below with roadTripCostBaseMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var roadTripCostComfortableMain = function (input) {
   // Complete the Comfortable: Road Trip Cost exercise below with roadTripCostComfortableMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var iceMachineMain = function (input) {
   // Complete the More Comfortable: Ice Machine exercise below with iceMachineMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var beerOrderMain = function (input) {
   // Complete the More Comfortable: Beer Order exercise below with beerOrderMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var cellularDataMain = function (input) {
   // Complete the More Comfortable: Cost of Cellular Data exercise below with cellularDataMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
--- script.js
+++ script.js
@@ -1,4 +1,117 @@
+// 1.  Remember! Global variables exist throughout the program
+// But variables with function scope ONLY exist inside the function when the function runs
+// Let's understand this code
+
+var papayaCount = 0;
+
+var main = function () {
+  papayaCount = papayaCount + 1;
+  var myOutputValue = "You have " + papayaCount + " papayas";
+  return myOutputValue;
+};
+
+//  Q: Why does papayaCount on line 5 have the var keyword and line 7 doesn't?
+
+//  Q: What behaviour would we see if we moved line 5 inside the main function?
+
+//  Q: What behaviour would we see if we moved line 5 below the main function?
+
+//  Q: Will moving line 9 above line 8 make a difference in how the program behaves?
+
+// ################################################################################
+
+// 2. Global Scope
+
+var papayaCount = 0;
+
+console.log("papaya count outside");
+console.log(papayaCount);
+
+var main = function () {
+  papayaCount = papayaCount + 1;
+
+  console.log("papaya count inside");
+  console.log(papayaCount);
+
+  var myOutputValue = "You have " + papayaCount + " papayas";
+  return myOutputValue;
+};
+
+// ################################################################################
+
+// 3. Function Scope
+
+var papayaCount = 0;
+
+console.log("myOutputValue outside");
+console.log(myOutputValue);
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  papayaCount = papayaCount + 1;
+
+  var myOutputValue = "You have " + papayaCount + " papayas";
+
+  console.log("myOutputValue inside");
+  console.log(myOutputValue);
+
+  return myOutputValue;
+};
+
+///////////////////////////////////////////////
+
+// Take user name in first game mode
+// Then play game in second game mode
+
+// Global variable
+// Used as Game state / programme state
+var mode = "input username";
+
+var userName = "";
+
+var main = function (input) {
+  console.log("input:", input);
+  console.log("mode:", mode);
+
+  var myOutputValue = "";
+
+  if (mode == "input username") {
+    userName = input;
+    mode = "play SPS";
+    myOutputValue = `Hi ${userName}, please enter Scissors, paper or stone to play`;
+  } else if (mode == "play SPS") {
+    // ..... SPS game logic and helper function
+    console.log("SPS Game,", input);
+    myOutputValue = `${userName}, you entered ${input} as your guess. You WON/LOST/DREW!`;
+  }
+
+  return myOutputValue;
+};
+
+///////////////////////////////////////////////
+
+// Used as Game state / programme state example
+var mode = "meat";
+
+var main = function (input) {
+  console.log("input:", input);
+
+  if (input == "i love meat") {
+    mode = "meat";
+  } else if (input == "i hate meat") {
+    mode = "veg";
+  }
+
+  console.log("mode:", mode);
+
+  var myOutputValue = "";
+
+  if (mode == "meat") {
+    myOutputValue = "Wow cheeseburger so goooood";
+  }
+
+  if (mode == "veg") {
+    myOutputValue = "GIVE ME THAT SALAD!";
+  }
+
   return myOutputValue;
 };
