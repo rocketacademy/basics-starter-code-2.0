b9c5df860544fc2525b3195cfd9be97577e2ae3a
douglashjf <40919449+douglashjf@users.noreply.github.com>
2022-04-04 21:30:15 +0800

    4 Apr SPS Base

--- day04-loops/pre-class/script.js
+++ day04-loops/pre-class/script.js
@@ -1,23 +1,60 @@
 var followAlongMain = function (input) {
   // Attempt the Follow Along exercise from the Loops module below with followAlongMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "";
   return myOutputValue;
 };
 
 var simpleLoopMain = function (input) {
   // Attempt the Simple Loop with Variations exercise from the Loops module below with simpleLoopMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "";
+  var counter = 0;
+  // Continue the loop while counter is less than the input value
+  while (counter < input) {
+    // Add 1 "yes" to the output for every loop iteration.
+    myOutputValue = myOutputValue + "hello";
+    counter = counter + 1;
+  }
   return myOutputValue;
 };
 
 var loopWithinLoopMain = function (input) {
   // Attempt the Loop Within Loop exercise from the Loops module below with loopWithinLoopMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "";
+
+  // Initialise the outer counter, rowCounter
+  var rowCounter = 0;
+
+  while (rowCounter < input) {
+    // Inside the outer loop, initialise the inner counter, columnCounter
+    var columnCounter = 0;
+
+    // Every time the outer loop runs, the inner loop runs repeatedly until
+    // the inner loop condition is met.
+    while (columnCounter < input) {
+      // Each time the inner loop runs, it adds "x" to output
+      myOutputValue = myOutputValue + "hello";
+      columnCounter = columnCounter + 1;
+    }
+
+    // At the end of each outer loop, add a <br> tag to begin a new row
+    myOutputValue = myOutputValue + "<br>";
+    rowCounter = rowCounter + 1;
+  }
+
+  // After the outer loop has run to completion, return the output compiled
+  // by the above loops.
   return myOutputValue;
 };
 
 var infiniteLoopMain = function (input) {
   // Attempt the Infinite Loop exercise from the Loops module below with infiniteLoopMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "";
+  var counter = 0;
+  // Continue the loop while counter is less than the input value
+  while (counter < input) {
+    // Add 1 "yes" to the output for every loop iteration.
+    myOutputValue = myOutputValue + "hello";
+    counter = counter + 0;
+  }
   return myOutputValue;
 };
