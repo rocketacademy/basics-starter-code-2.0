50003a6f8a3cebf8b0d386642a0b14c3458bb6e0
amyhuimin <101979602+amyhuimin@users.noreply.github.com>
2022-04-04 23:13:16 +0900

    initial commit

--- day01-data-manipulation-and-functions/in-class/script.js
+++ day01-data-manipulation-and-functions/in-class/script.js
@@ -1,35 +1,51 @@
 var fahrenheitToCelsiusMain = function (input) {
-  // Complete the Base: Fahrenheit to Celsius exercise below with fahrenheitToCelsiusMain as the main function.
-  var myOutputValue = 'hello world';
+  // Formula for Fahrenheit to Celsius is "Celcius = (Fahrenheit-32) x 5/9".
+  var celsius = ((input - 32) * 5) / 9;
+  var myOutputValue =
+    "So, " + input + " Fahrenheit is " + celsius + " celsius.";
   return myOutputValue;
 };
 
 var roadTripCostBaseMain = function (input) {
-  // Complete the Base: Road Trip Cost exercise below with roadTripCostBaseMain as the main function.
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  // First, convert km to miles.
+  var distanceInMiles = input * 0.62;
+  //Second, find out how many litres needed
+  var litresNeeded = distanceInMiles / 9;
+  //Then, take litres * $2.20
+  var cost = litresNeeded * 2.2;
+  return cost;
 };
 
 var roadTripCostComfortableMain = function (input) {
-  // Complete the Comfortable: Road Trip Cost exercise below with roadTripCostComfortableMain as the main function.
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  // First, convert km to miles
+  var distanceInMiles = input * 0.62;
+  // Find out how many litres needed
+  var litresNeeded = distanceInMiles / 9;
+  // Cost savings for fuel is 2.20 - 2.00 = 0.20
+  var fuelSavings = litresNeeded * 0.2;
+  return fuelSavings;
 };
 
 var iceMachineMain = function (input) {
-  // Complete the More Comfortable: Ice Machine exercise below with iceMachineMain as the main function.
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  // Input number of guests
+  // Each guest needs 8 * 1.5 = 12 grams of ice
+  // Assume 100 guests, 100 * 12 = 1200g of ice
+  // convert grams to pounds
+  // Divide pounds by 5 = hours needed
+  var gramsIceNeeded = input * 12;
+  var pounds = gramsIceNeeded * 0.00220462;
+  var hoursNeeded = pounds / 5;
+  return hoursNeeded;
 };
 
 var beerOrderMain = function (input) {
   // Complete the More Comfortable: Beer Order exercise below with beerOrderMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var cellularDataMain = function (input) {
   // Complete the More Comfortable: Cost of Cellular Data exercise below with cellularDataMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
--- script.js
+++ script.js
@@ -1,4 +1,154 @@
+//Create functions for game modes
+//Ask for player's name
+//Greet player. Ask player to choose game mode
+//Ask player to input choice
+//Computer to choose
+//Compare results and display results according to game mode.
+var currentGameMode = "ask for username";
+var winningTimes = 0;
+var losingTimes = 0;
+var gamesPlayed = 0;
+var userName = "";
+var myOutputValue = "";
+//var winningPercentage = (winningTimes / gamesPlayed) * 100; WHY IS IT DISPLAYING NAN
+//Start of normal game function
+var playNormalGame = function (userName, guess) {
+  if (
+    guess != "scissors" ||
+    guess != "paper" ||
+    guess != "stone" ||
+    guess != "reversed scissors" ||
+    guess != "reversed paper" ||
+    guess != "reversed stone"
+  ) {
+    var myOutputValue = "This is not a valid choice.";
+  }
+  //start of reversed game function
+  var reversed = function () {
+    var compChoice = Math.ceil(Math.random() * 3);
+    if (compChoice == 1) {
+      compChoice = "reversed scissors";
+    }
+    if (compChoice == 2) {
+      compChoice = "reversed paper";
+    }
+    if (compChoice == 3) {
+      compChoice = "reversed stone";
+    }
+    //compare player's guess to computer's guess and show results
+    if (guess == compChoice) {
+      console.log("guess is compchoice");
+      console.log(compChoice);
+      console.log(guess);
+      message = `You chose ${guess}. <br>The computer chose ${compChoice}. <br>It's a draw. <br>You have played ${gamesPlayed} times, and you have won ${winningTimes} times and lost ${losingTimes} times.`;
+    } else if (guess == "reversed scissors") {
+      console.log(compChoice);
+      console.log(guess);
+      if (compChoice == "reversed stone") {
+        winningTimes += 1;
+        message = `You chose ${guess}. <br>The computer chose ${compChoice}. <br>You win! <br>You have played ${gamesPlayed} times, and you have won ${winningTimes} times and lost ${losingTimes} times.`;
+      } else if (compChoice == "reversed paper") {
+        losingTimes += 1;
+        message = `You chose ${guess}??. <br>The computer chose ${compChoice}. <br>You lose. <br>You have played ${gamesPlayed} times, and you have won ${winningTimes} times and lost ${losingTimes} times.`;
+      }
+    } else if (guess == "reversed paper") {
+      console.log(compChoice);
+      console.log(guess);
+      if (compChoice == "reversed stone") {
+        losingTimes += 1;
+        message = `You chose ${guess}??. <br>The computer chose ${compChoice}. <br>You lose. <br>You have played ${gamesPlayed} times, and you have won ${winningTimes} times and lost ${losingTimes} times.`;
+      } else if (compChoice == "reversed scissors") {
+        winningTimes += 1;
+        message = `You chose ${guess}. <br>The computer chose ${compChoice}. <br>You win! <br>You have played ${gamesPlayed} times, and you have won ${winningTimes} times and lost ${losingTimes} times.`;
+      }
+    } else if (guess == "reversed stone") {
+      console.log(compChoice);
+      console.log(guess);
+      if (compChoice == "reversed scissors") {
+        losingTimes += 1;
+        message = `You chose ${guess}. <br>The computer chose ${compChoice}. <br>You lose. <br>You have played ${gamesPlayed} times, and you have won ${winningTimes} times and lost ${losingTimes} times.`;
+      } else if (compChoice == "reversed paper") {
+        winningTimes += 1;
+        message = `You chose ${guess}. <br>The computer chose ${compChoice}. <br>You win! <br>You have played ${gamesPlayed} times, and you have won ${winningTimes} times and lost ${losingTimes} times.`;
+      }
+    }
+  };
+  //end of reversed function
+  if (guess.includes("reversed")) {
+    console.log("do something special because text contains reversed");
+    reversed();
+  }
+
+  gamesPlayed += 1;
+  //let computer choose randomly
+  var compChoice = Math.ceil(Math.random() * 3);
+  if (compChoice == 1) {
+    compChoice = "scissors";
+  }
+  if (compChoice == 2) {
+    compChoice = "paper";
+  }
+  if (compChoice == 3) {
+    compChoice = "stone";
+  }
+  //compare player's guess to computer's guess and show results
+  if (guess == compChoice) {
+    console.log("guess is " + compChoice);
+    console.log(compChoice);
+    console.log(guess);
+    var message = `You chose ${guess}. <br>The computer chose ${compChoice}. <br>It's a draw.<br>You have played ${gamesPlayed} times, and you have won ${winningTimes} times and lost ${losingTimes} times.`;
+  } else if (guess == "scissors") {
+    console.log(compChoice);
+    console.log(guess);
+    if (compChoice == "stone") {
+      losingTimes += 1;
+      message = `You chose ${guess}. <br>The computer chose ${compChoice}. <br>You lose.<br>You have played ${gamesPlayed} times, and you have won ${winningTimes} times and lost ${losingTimes} times.`;
+    } else if (compChoice == "paper") {
+      winningTimes += 1;
+      message = `You chose ${guess}. <br>The computer chose ${compChoice}. <br>You win!<br>You have played ${gamesPlayed} times, and you have won ${winningTimes} times and lost ${losingTimes} times.`;
+    }
+  } else if (guess == "paper") {
+    console.log(compChoice);
+    console.log(guess);
+    if (compChoice == "stone") {
+      winningTimes += 1;
+      message = `You chose ${guess}:spiral_note_pad:. <br>The computer chose ${compChoice}. <br>You win!<br>You have played ${gamesPlayed} times, and you have won ${winningTimes} times and lost ${losingTimes} times.`;
+    } else if (compChoice == "scissors") {
+      losingTimes += 1;
+      message = `You chose ${guess}:spiral_note_pad:. <br>The computer chose ${compChoice}. <br>You lose.<br>You have played ${gamesPlayed} times, and you have won ${winningTimes} times and lost ${losingTimes} times.`;
+    }
+  } else if (guess == "stone") {
+    console.log(compChoice);
+    console.log(guess);
+    if (compChoice == "scissors") {
+      winningTimes += 1;
+      message = `You chose ${guess}. <br>The computer chose ${compChoice}. <br>You win!<br>You have played ${gamesPlayed} times, and you have won ${winningTimes} times and lost ${losingTimes} times.`;
+    } else if (compChoice == "paper") {
+      losingTimes += 1;
+      message = `You chose ${guess}. <br>The computer chose ${compChoice}. <br>You lose. <br>You have played ${gamesPlayed} times, and you have won ${winningTimes} times and lost ${losingTimes} times.`;
+    }
+  }
+  return message;
+};
+//End of normal game function
+//Function to ask for username
+var askName = function (input) {
+  var askNameMessage = "Hi! What's your name?";
+  return askNameMessage;
+};
+//END function to ask username.
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "";
+  if (currentGameMode == "ask for username") {
+    askName();
+    // set the name
+    userName = input;
+    // now that we have the name, switch the mode
+    currentGameMode = "play game";
+    myOutputValue = `Hello ${userName}! <br>Please type "scissors", "paper" or "stone" (without quotation marks) to start! <br><br>Or, type "reversed scissors", "reversed paper" or "reversed stone" to play the reverse game!`;
+  } else if (currentGameMode == "play game") {
+    // refactored function that can be reused anywhere
+    myOutputValue = playNormalGame(userName, input);
+  }
   return myOutputValue;
 };
